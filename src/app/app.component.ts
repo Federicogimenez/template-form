917ee04715fabe6bf9a78df273b714e76bd9ec72
Federico <gimanezf@gmail.com>
2022-07-21 17:39:14 -0300

    first commit form-changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'template-form-fede';
+
+  public data = false
+
+  public login: LoginFormBig = {
+    email: '',
+    password: '',
+    repassword:'',
+    adress: '',
+    postalCode: '', 
+    celPhoneNumber: ''
+  }
+  public onSubmit() {
+    console.log('data enviada')
+    this.data = true
+  }
+}
+
+interface LoginFormBig{
+  email: string,
+  password: string,
+  repassword: string,
+  adress: string,
+  postalCode: string, 
+  celPhoneNumber: string
 }
